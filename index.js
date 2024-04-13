f94075f073390f7fbb46d572ec350a059446d29e
TheBeastLT <pauliox@beyond.lt>
2024-04-13 18:42:08 +0300

    expose common config params to allow changing them

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*! bittorrent-tracker. MIT License. WebTorrent LLC <https://webtorrent.io/opensource> */
 import Client from './client.js'
 import Server from './server.js'
+import common from './lib/common.js'
 
 export default Client
-export { Client, Server }
+export { Client, Server, common }
